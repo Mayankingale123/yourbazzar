a0da3c51dcc90304f39c17aefc4351ccf4889d7e
Mihir-solanki-13 <13mihir2001@gmail.com>
2023-11-16 22:44:37 +0530

    update email sending address

--- base/emails.py
+++ base/emails.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 def send_account_activation_email(email , email_token):
     subject = 'Your account needs to be verified'
     email_from = settings.EMAIL_HOST_USER
-    message = f'Hi, click on the link to activate your account https://ecommerce-qdop.onrender.com/accounts/activate/{email_token}'
+    message = f'Hi, click on the link to activate your account https://yourbazzar.onrender.com/accounts/activate/{email_token}'
     send_mail(subject , message , email_from , [email])
